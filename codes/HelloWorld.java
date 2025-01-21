43464e892559feaa013b8696609d939199f7d514
ttknpde-v <thitikorn_nupan@outlook.co.th>
2025-01-21 08:29:36 +0700

    modified commit by don

--- codes/HelloWorld.java
+++ codes/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i+". Hello World");
+        }
     }
 }
